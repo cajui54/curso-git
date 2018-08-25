67465fd08e97500f146890b914afb9be004890ce
Jackson Siva <cajui54@hotmail.com>
2018-08-25 12:53:51 -0300

    Acrescentada a mensagem Desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -11,8 +11,9 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
+            
             Console.WriteLine("Como vc está hoje?");
+            Console.WriteLine("tenha um bom dia!");
             Console.ReadLine();
         }
     }
